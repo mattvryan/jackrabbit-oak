f7d83c9cc4ed74f3419f87de780be8e7a4537e5d
Matt Ryan <matt@mvryan.org>
2018-04-04 16:38:39 -0600

    Updated path restriction preference test.
    Test still isn't working corrctly but has the indexes and plans
    which are being used by the query engine.  It isn't actually
    searching the content so I can't verify results.

--- oak-core/src/test/java/org/apache/jackrabbit/oak/query/PathRestrictionPreferenceTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/query/PathRestrictionPreferenceTest.java
@@ -1,6 +1,7 @@
 package org.apache.jackrabbit.oak.query;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 import org.apache.jackrabbit.oak.InitialContent;
 import org.apache.jackrabbit.oak.Oak;
 import org.apache.jackrabbit.oak.api.CommitFailedException;
@@ -8,6 +9,7 @@ import org.apache.jackrabbit.oak.api.ContentRepository;
 import org.apache.jackrabbit.oak.api.ContentSession;
 import org.apache.jackrabbit.oak.api.PropertyState;
 import org.apache.jackrabbit.oak.api.QueryEngine;
+import org.apache.jackrabbit.oak.api.Result;
 import org.apache.jackrabbit.oak.api.Root;
 import org.apache.jackrabbit.oak.api.Type;
 import org.apache.jackrabbit.oak.plugins.index.property.PropertyIndexPlan;
@@ -31,6 +33,7 @@ import javax.annotation.CheckForNull;
 import javax.annotation.Nonnull;
 import javax.jcr.NoSuchWorkspaceException;
 import javax.security.auth.login.LoginException;
+import java.text.ParseException;
 import java.util.List;
 
 import static junit.framework.TestCase.fail;
@@ -42,11 +45,15 @@ public class PathRestrictionPreferenceTest {
 
     private static int nChildren = 5;
     private static int nLevels = 5;
-    private static int nodeCount = 5^5+5^4+5^3+5^2+5^1;
+    private static int nodeCount = (int)(Math.pow(5,5)+Math.pow(5,4)+Math.pow(5,3)+Math.pow(5,2)+5);
+
+    private static final String NODE_CONTENT = "content";
+    private static final String PROP_COUNTER = "counter";
+    private static final String PROP_CHARGE = "charge";
 
     @Test
     public void testSimple() throws LoginException, NoSuchWorkspaceException,
-            CommitFailedException {
+            CommitFailedException, ParseException {
         NodeStore nodeStore = new MemoryNodeStore();
         Oak oak = new Oak(nodeStore)
                 .with(new OpenSecurityProvider())
@@ -55,55 +62,41 @@ public class PathRestrictionPreferenceTest {
                 ;
         ContentRepository repo = oak.createContentRepository();
         ContentSession session = repo.login(null, null);
-        Root root = session.getLatestRoot();
-        QueryEngine queryEngine = root.getQueryEngine();
 
         NodeBuilder rootBuilder = nodeStore.getRoot().builder();
-        NodeBuilder contentNode = rootBuilder.child("content");
+        NodeBuilder contentNode = rootBuilder.child(NODE_CONTENT);
         createContentTree(contentNode, nChildren, nLevels, true);
         mergeAndWait(nodeStore, rootBuilder);
 
-        for (ChildNodeEntry entry : nodeStore.getRoot().getChildNodeEntries()) {
-            if (entry.getName() == "content") {
-                traverseBranch(entry, "");
-            }
-        }
+        QueryString counterQuery = query().where(PROP_COUNTER, 0).path(NODE_CONTENT).explain();
+        QueryString chargeQuery = query().where(PROP_CHARGE, false).path(NODE_CONTENT);
 
-        String numberQuery = "select * from [nt:base] where [number] = 0";
-        String polarityQuery = "select * from [nt:base] where [polarity] = cast('false' as BOOLEAN)";
-
-//        for (String query : Lists.newArrayList(numberQuery, polarityQuery)) {
-//            try {
-//                Result result = queryEngine.executeQuery(query,
-//                        QueryEngineImpl.SQL2,
-//                        QueryEngine.NO_BINDINGS,
-//                        QueryEngine.NO_MAPPINGS);
-//                logger.info(result.toString());
-//            } catch (ParseException e) {
-//                fail(e.getMessage());
-//            }
-//        }
+        Root root = session.getLatestRoot();
+        QueryEngine queryEngine = root.getQueryEngine();
+        Result result = queryEngine.executeQuery(counterQuery.toString(),
+                QueryEngineImpl.SQL2,
+                QueryEngine.NO_BINDINGS,
+                QueryEngine.NO_MAPPINGS);
     }
 
-    private void createContentTree(NodeBuilder builder, int nChildren, int nLevels, boolean b) {
+    private void createContentTree(NodeBuilder builder, int nChildren, int nLevels, boolean charge) {
         if (0 == nLevels || 0 == nChildren) return;
         for (int i=0; i<nChildren; i++) {
             NodeBuilder child = builder.child(String.format("Node_%d", i));
-            child.setProperty("a", i);
-            child.setProperty("b", b);
-            b = ! b;
-            createContentTree(child, nChildren, nLevels-1, b);
+            child.setProperty(PROP_COUNTER, i);
+            child.setProperty(PROP_CHARGE, charge);
+            charge = ! charge;
+            createContentTree(child, nChildren, nLevels-1, charge);
         }
     }
 
     private void traverseBranch(ChildNodeEntry node, String indent) {
-        PropertyState nodeAProp = node.getNodeState().getProperty("a");
-        String nodeNumber = nodeAProp != null ? Long.toString(nodeAProp.getValue(Type.LONG)) : "<none>";
-        PropertyState nodeBProp = node.getNodeState().getProperty("b");
-        String nodePolarity = nodeBProp != null ?
-                (nodeBProp.getValue(Type.BOOLEAN) ? "+" : "-")
-                : "<none>";
-        logger.info(String.format("%s%s - N: %s, P: %s", indent, node.getName(), nodeNumber, nodePolarity));
+        PropertyState counterProp = node.getNodeState().getProperty(PROP_COUNTER);
+        String counter = counterProp != null ? Long.toString(counterProp.getValue(Type.LONG)) : "<none>";
+        PropertyState chargeProp = node.getNodeState().getProperty(PROP_CHARGE);
+        String charge = chargeProp != null ?
+                (chargeProp.getValue(Type.BOOLEAN) ? "+" : "-") : "<none>";
+        logger.info(String.format("%s%s - Counter: %s, Charge: %s", indent, node.getName(), counter, charge));
         for (ChildNodeEntry child : node.getNodeState().getChildNodeEntries()) {
             traverseBranch(child, indent+"    ");
         }
@@ -122,6 +115,7 @@ public class PathRestrictionPreferenceTest {
         try {
             int nTries = 0;
             while (! commitHappened && nTries < 10) {
+                nTries++;
                 Thread.sleep(100);
             }
         }
@@ -131,19 +125,85 @@ public class PathRestrictionPreferenceTest {
         }
     }
 
+    private QueryString query() {
+        return new QueryString("select * from [nt:base]");
+    }
+
+    private static class QueryString {
+        private String query;
+
+        QueryString(String select) {
+            query = String.format("%s as a", select);
+        }
+
+        @Override
+        public String toString() {
+            return query;
+        }
+
+        QueryString where(String propertyName, int value) {
+            query = String.format("%s where [%s] = %d", query, propertyName, value);
+            return this;
+        }
+
+        QueryString where(String propertyName, boolean value) {
+            query = String.format("%s where [%s] = %b", query, propertyName, value);
+            return this;
+        }
+
+        QueryString path(String pathRestriction) {
+            query = String.format("%s and isdescendantnode(a, '/%s')", query, pathRestriction);
+            return this;
+        }
+
+        QueryString explain() {
+            query = String.format("explain %s", query);
+            return this;
+        }
+    }
+
     private static class TestIndexProvider implements QueryIndexProvider {
-        QueryIndex aIndex = new AIndex();
-        QueryIndex bIndex = new BIndex();
+        QueryIndex counterIndex = new CounterIndex();
+        QueryIndex chargeIndex = new ChargeIndex();
         @Override
         public List<? extends QueryIndex> getQueryIndexes(NodeState nodeState) {
-            return ImmutableList.of(aIndex, bIndex);
+            return ImmutableList.of(counterIndex, chargeIndex);
         }
     }
 
     private static abstract class AbstractTestIndex
             implements QueryIndex, QueryIndex.AdvancedQueryIndex {
+        IndexPlan indexPlan;
+        String indexName;
+
+        protected AbstractTestIndex(IndexPlan indexPlan, String indexName) {
+            this.indexPlan = indexPlan;
+            this.indexName = indexName;
+        }
+
         @Override
         public double getMinimumCost() { return PropertyIndexPlan.COST_OVERHEAD + 0.1; }
+
+        @Override
+        public String getPlan(Filter filter, NodeState rootState) {
+            return indexPlan.getPlanName();
+        }
+
+        @Override
+        public String getIndexName() {
+            return indexName;
+        }
+
+        @Override
+        public List<IndexPlan> getPlans(Filter filter, List<OrderEntry> sortOrder, NodeState rootState) {
+            indexPlan.setFilter(filter);
+            return Lists.newArrayList(indexPlan);
+        }
+
+        @Override
+        public String getPlanDescription(IndexPlan plan, NodeState root) {
+            return null;
+        }
 
         @Override
         public Cursor query(Filter filter, NodeState rootState) { return null; }
@@ -152,63 +212,49 @@ public class PathRestrictionPreferenceTest {
         public Cursor query(IndexPlan plan, NodeState rootState) { return null; }
     }
 
-    private static class AIndex extends AbstractTestIndex {
+    private static class CounterIndex extends AbstractTestIndex {
+        CounterIndex() {
+            super(new CounterIndexPlan(), "counter-index");
+        }
+
         @Override
         public double getCost(Filter filter, NodeState rootState) {
-            return null != filter.getPropertyRestriction("a") ? 100 : Double.POSITIVE_INFINITY;
-        }
-
-        @Override
-        public String getPlan(Filter filter, NodeState rootState) { return null; }
-
-        @Override
-        public String getIndexName() { return "a-index"; }
-
-        @Override
-        public List<IndexPlan> getPlans(Filter filter, List<OrderEntry> sortOrder, NodeState rootState) {
-            return null;
-        }
-
-        @Override
-        public String getPlanDescription(IndexPlan plan, NodeState root) {
-            return null;
+            return null != filter.getPropertyRestriction(PROP_COUNTER) ? 100 : Double.POSITIVE_INFINITY;
         }
     }
 
-    private static class BIndex extends AbstractTestIndex {
+    private static class ChargeIndex extends AbstractTestIndex {
+        ChargeIndex() {
+            super(new ChargeIndexPlan(), "charge-index");
+        }
+
         @Override
         public double getCost(Filter filter, NodeState rootState) {
-            return null != filter.getPropertyRestriction("b") ? 500 : Double.POSITIVE_INFINITY;
-        }
-
-        @Override
-        public String getPlan(Filter filter, NodeState rootState) { return null; }
-
-        @Override
-        public String getIndexName() { return "b-index"; }
-
-        @Override
-        public List<IndexPlan> getPlans(Filter filter, List<OrderEntry> sortOrder, NodeState rootState) {
-            return null;
-        }
-
-        @Override
-        public String getPlanDescription(IndexPlan plan, NodeState root) {
-            return null;
+            return null != filter.getPropertyRestriction(PROP_CHARGE) ? 500 : Double.POSITIVE_INFINITY;
         }
     }
 
     private static abstract class AbstractTestIndexPlan implements QueryIndex.IndexPlan {
         private Filter filter = null;
+        protected String planName;
 
-        @Override
-        public double getCostPerExecution() {
-            return 2;
+        protected AbstractTestIndexPlan(String planName) {
+            this.planName = planName;
         }
 
         @Override
+        public double getCostPerExecution() {
+            if (filterMatchesPlan(filter)) {
+                return 2.0;
+            }
+            return Double.POSITIVE_INFINITY;
+        }
+
+        protected abstract boolean filterMatchesPlan(Filter filter);
+
+        @Override
         public double getCostPerEntry() {
-            return 1;
+            return 1.0;
         }
 
         @Override
@@ -267,41 +313,50 @@ public class PathRestrictionPreferenceTest {
         public Object getAttribute(String name) {
             return null;
         }
+
+        @CheckForNull
+        @Override
+        public String getPlanName() {
+            return planName;
+        }
+
+        @Override
+        public boolean getSupportsPathRestriction() {
+            return (null != filter) && (null != filter.getPathRestriction()) &&
+                    (0 == filter.getPathRestriction().compareTo(Filter.PathRestriction.ALL_CHILDREN) &&
+                    filter.getPath().equals(String.format("/%s", NODE_CONTENT)));
+        }
     }
 
-    private static class AIndexPlan extends AbstractTestIndexPlan {
+    private static class CounterIndexPlan extends AbstractTestIndexPlan {
+        CounterIndexPlan() {
+            super("counter-index-plan");
+        }
+
         @Override
         public long getEstimatedEntryCount() {
             return (long)(nodeCount / (nChildren*2));
         }
 
         @Override
-        public boolean getSupportsPathRestriction() {
-            return false;
-        }
-
-        @CheckForNull
-        @Override
-        public String getPlanName() {
-            return "a-index-plan";
+        protected boolean filterMatchesPlan(Filter filter) {
+            return null != filter && null != filter.getPropertyRestriction(PROP_COUNTER);
         }
     }
 
-    private static class BIndexPlan extends AbstractTestIndexPlan {
+    private static class ChargeIndexPlan extends AbstractTestIndexPlan {
+        ChargeIndexPlan() {
+            super("charge-index-plan");
+        }
+
         @Override
         public long getEstimatedEntryCount() {
             return (long)(nodeCount / 2);
         }
 
         @Override
-        public boolean getSupportsPathRestriction() {
-            return false;
-        }
-
-        @CheckForNull
-        @Override
-        public String getPlanName() {
-            return "b-index-plan";
+        protected boolean filterMatchesPlan(Filter filter) {
+            return null != filter && null != filter.getPropertyRestriction(PROP_CHARGE);
         }
     }
 }
