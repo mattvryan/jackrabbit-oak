7bc746909b2fdf42611e22e35398bc58ad00f2c6
Matt Ryan <oss@mvryan.org>
2018-11-08 03:46:49 -0700

    Adds retry to bucket creation code in init().
    This code is to allow tolerance for an HTTP 409 response code
    from AWS.  During CI testing it is possible to see some tests
    fail due to a 409 being returned from AWS, probably due to
    conflicts where one system is trying to create a bucket that is
    being deleted or created by another system.

--- oak-blob-cloud/src/main/java/org/apache/jackrabbit/oak/blob/cloud/s3/S3Backend.java
+++ oak-blob-cloud/src/main/java/org/apache/jackrabbit/oak/blob/cloud/s3/S3Backend.java
@@ -17,6 +17,11 @@
 
 package org.apache.jackrabbit.oak.blob.cloud.s3;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.collect.Iterables.filter;
+import static java.lang.Thread.currentThread;
+import static java.lang.Thread.sleep;
+
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
@@ -45,26 +50,7 @@ import com.amazonaws.HttpMethod;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.S3ClientOptions;
-import com.amazonaws.services.s3.model.BucketAccelerateConfiguration;
-import com.amazonaws.services.s3.model.CompleteMultipartUploadRequest;
-import com.amazonaws.services.s3.model.CopyObjectRequest;
-import com.amazonaws.services.s3.model.DeleteObjectsRequest;
-import com.amazonaws.services.s3.model.DeleteObjectsResult;
-import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
-import com.amazonaws.services.s3.model.GetBucketAccelerateConfigurationRequest;
-import com.amazonaws.services.s3.model.InitiateMultipartUploadRequest;
-import com.amazonaws.services.s3.model.InitiateMultipartUploadResult;
-import com.amazonaws.services.s3.model.ListObjectsRequest;
-import com.amazonaws.services.s3.model.ListPartsRequest;
-import com.amazonaws.services.s3.model.ObjectListing;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PartETag;
-import com.amazonaws.services.s3.model.PartListing;
-import com.amazonaws.services.s3.model.PartSummary;
-import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.amazonaws.services.s3.model.Region;
-import com.amazonaws.services.s3.model.S3Object;
-import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.amazonaws.services.s3.model.*;
 import com.amazonaws.services.s3.transfer.Copy;
 import com.amazonaws.services.s3.transfer.TransferManager;
 import com.amazonaws.services.s3.transfer.Upload;
@@ -78,6 +64,7 @@ import com.google.common.collect.AbstractIterator;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpStatus;
 import org.apache.jackrabbit.core.data.DataIdentifier;
 import org.apache.jackrabbit.core.data.DataRecord;
 import org.apache.jackrabbit.core.data.DataStoreException;
@@ -91,10 +78,6 @@ import org.apache.jackrabbit.oak.spi.blob.AbstractSharedBackend;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import static com.google.common.base.Preconditions.checkArgument;
-import static com.google.common.collect.Iterables.filter;
-import static java.lang.Thread.currentThread;
 
 /**
  * A data store backend that stores data on Amazon S3.
@@ -189,12 +172,24 @@ public class S3Backend extends AbstractSharedBackend {
                 }
             }
 
-            if (!s3service.doesBucketExist(bucket)) {
-                s3service.createBucket(bucket, s3Region);
-                LOG.info("Created bucket [{}] in [{}] ", bucket, region);
-            } else {
-                LOG.info("Using bucket [{}] in [{}] ", bucket, region);
+            CreateBucketRequest createBucketRequest = new CreateBucketRequest(bucket, s3Region);
+            int retries = 0;
+            while (!s3service.doesBucketExist(bucket)) {
+                try {
+                    s3service.createBucket(createBucketRequest);
+                    LOG.info("Created bucket [{}] in [{}] ", bucket, region);
+                    break;
+                }
+                catch (AmazonS3Exception e) {
+                    if (HttpStatus.SC_CONFLICT == e.getStatusCode() && 10 < ++retries) {
+                        sleep(100);
+                    }
+                    else {
+                        throw e;
+                    }
+                }
             }
+            LOG.info("Using bucket [{}] in [{}] ", bucket, region);
 
             int writeThreads = 10;
             String writeThreadsStr = properties.getProperty(S3Constants.S3_WRITE_THREADS);
